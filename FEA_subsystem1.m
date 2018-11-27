63a2fd7dce46f6ad349ead885256ba84e2c8d5ac
HeewonMin <45391282+HeewonMin@users.noreply.github.com>
2018-11-27 21:14:05 +0900

    Update FEA_subsystem1.m

--- FEA_subsystem1.m
+++ FEA_subsystem1.m
@@ -2,6 +2,7 @@ function Performance = FEA_subsystem1( Var )
     
 
     Const{1} = @(x) 1-x(:,1).^2.*x(:,2)./20; % Constraint 1
+    hahaha! 
     Const{2} = @(x) 1-(80./(x(:,1).^2+8*x(:,2)+5)); % Constraint 2
     Const{3} = @(x) -1 + (0.9063*x(:,1)+0.4226*x(:,2)-6).^2+(0.9063*x(:,1)+0.4226*x(:,2)-6).^3-0.6*(0.9063*x(:,1)+0.4226*x(:,2)-6).^4-(-0.4226*x(:,1)+0.9063*x(:,2));
     LinkingConst{1} = @(x) (2*x(:,1).^2-x(:,2))./10; % Linking variable
